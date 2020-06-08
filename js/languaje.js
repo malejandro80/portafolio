53e789bcd0d3b02dd7538d42db593e35d5fcd32a
miguel bastidas <m.alejandro_80@hotmail.com>
2020-06-07 23:22:52 -0400

    changes

--- js/languaje.js
+++ js/languaje.js
@@ -13,7 +13,7 @@ const lang = {
     email: 'Email',
     phone: 'Phone',
     skills: 'Skills',
-    used_use: 'I used to use',
+    used_use: 'Tools i have worked',
     about_message:
       "Hello, my name is Miguel and it's a pleasure to greet you, I've been in the world of programming for about 4 years. I started in university where I studied mechanical engineering, basic programming was in the curriculum, I liked it so much that I decided to continue learning on my own reading books, magazines, blogs, taking courses on various online education platforms and doing my own projects. Professionally, I have been working for 3 years where I have worked in several renowned applications both nationally and internationally, I have worked for agencies and also independently. Not all my hobbies are based solely on programming, I am also interested in science, physics, mechanics, industrial automation, arts and music. In short I am curious by nature that is the great virtue that God gave me.",
     frontend_develop: 'FRONTEND DEVELOP',
@@ -26,9 +26,9 @@ const lang = {
     label_develop: 'en desarrollo',
     label_end: 'Finalizado',
     label_learning: 'Periodo de aprendizaje',
-    find_me: 'Find Me',
+    find_me: 'Contact Me',
     find_me_msj:
-      'You can find me through my social media, write me to know more!',
+      'Follow me on my social media or write to me to find out more, I will be happy to answer you.',
     cite:
       '"My north will always be a clean and excellent work, together with values of responsibility, commitment and dedication the result will always be the satisfaction of my clients"',
     use_tech: 'Tech i used here:',
@@ -45,7 +45,7 @@ const lang = {
     email: 'Correo',
     phone: 'Telefono',
     skills: 'Habilidades',
-    used_use: 'Herramientas que suelo usar',
+    used_use: 'Herramientas con las que he trabajado.',
     about_message:
       'Hola, me llamo miguel y es un gusto saludarte, llevo aproximadamente en el mundo de la programación 4 años. Comencé en la universidad donde estudie ingeniería mecánica, programación básica estaba en el pensum de estudio, Me gusto tanto que decidí seguir aprendiendo por mi cuenta leyendo libros, revistas, blogs, haciendo cursos en diversas plataformas de educación online y realizando proyectos propios. Profesionalmente ya tengo 3 anos laborando donde he trabajos en varias aplicaciones de renombre tanto a nivel nacional como internacional, he trabajado para agencias y también de manera independiente. No todos mis gustos se basan solo en programar, también tengo interés en la ciencia, la física, mecánica, automatización industrial artes y musica. En resumen soy curioso por naturaleza esa es la gran virtud que dios me dio.',
     frontend_develop: 'DESARROLLO FRONTEND',
@@ -58,9 +58,9 @@ const lang = {
     label_develop: 'en desarrollo',
     label_end: 'Finalizado',
     label_learning: 'Periodo de aprendizaje',
-    find_me: 'Encuéntrame',
+    find_me: 'Contáctame',
     find_me_msj:
-      'Puedes conseguirme por mis redes sociales, ¡escríbeme para saber más!',
+      'Sígueme en mis redes sociales o escríbeme para saber más, con gusto te respondere.',
     cite:
       '"Mi norte siempre será un trabajo limpio y de excelencia, junto con valores de responsabilidad, compromiso y dedicación el resultado siempre será la satisfacción de mis clientes"',
     use_tech: 'Tecnologias Utilizadas:',
